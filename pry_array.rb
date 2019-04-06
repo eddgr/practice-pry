49010a2d1e5ce659aa9d5729e792b5e4ef097556
Edgar Ong <edgar@minus.com>
2019-04-06 10:32:52 -0400

    created Hash questions

--- pry_array.rb
+++ pry_array.rb
@@ -1,14 +1,14 @@
 require 'pry'
 
+fruits = ["bananas", "apples", "oranges", ["strawberries", "raspberries", ["tomato"]]]
+
+# How can we get the return_the_tomato method working?
+
 def return_the_tomato(array)
   binding.pry
 end
 
-fruits = ["bananas", "apples", "oranges", ["strawberries", "raspberries", ["tomato"]]]
-
 return_the_tomato(fruits)
-
-# How can we get the return_the_tomato method working?
 
 # How can we create a new method that returns "strawberries"?
 
---
+++ pry_hash.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+foods = {
+  healthy: ["apples", "granola bars", "oatmeal", super_healthy: ["kale chips", "dark chocolate"]],
+  snacky: ["doritos", "kitkat", "gummy worms", super_snacky: ["party mix", "popcorn"]]
+}
+
+binding.pry
+
+# How would I return the hash of :super_healthy?
+
+# How can I return the first item in :super_snacky?
+
+# Create a method to return a new array with all the items in :super_healthy and :super_snacky
